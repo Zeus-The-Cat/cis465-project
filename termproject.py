bb83c5c66ecc85474aac62d083a00dbe11b5bff6
Dakotah Pettry <8905735+Zeus-The-Cat@users.noreply.github.com>
2021-11-24 11:40:48 -0500

    Added output to file capabilities

--- termproject.py
+++ termproject.py
@@ -5,7 +5,7 @@ from skimage.util import noise, random_noise
 from skimage.metrics import structural_similarity
 
 # Eventually we can perform sigma_psd optimization on different types of noisy images
-# Have to make sure we display what's going on in terminal as these steps can take a 
+# Have to make sure we display what's going on in terminal as these steps can take a
 # ---- long time to process
 
 # Normalizes img at fileName, repeatedly calls compareBM3D() to find optimal sigma_psd
@@ -19,8 +19,8 @@ def processImage(file_name,noise_types,starting_sigma=0.2,starting_noise_varianc
 
     for index,noisy_image in enumerate(noisy_images):
         print('Processing '+noise_types[index]+' noise at '+str(starting_noise_variance) +' variance')
-        compareBM3D(noisy_image,img,sigma=starting_sigma,noise_type=noise_types[index])
-
+        fimg = compareBM3D(noisy_image,img,sigma=starting_sigma,noise_type=noise_types[index])
+        cv2.imwrite(noise_types[index]+str(starting_noise_variance)+'_sig'+fimg[1]+'_'+file_name,fimg[0])
     return 1.0
 
 # Applies BM3D with initial sigma_psd=0.2,compares sigma_psd=0.1,and sigma_pds=0.3
@@ -43,10 +43,10 @@ def compareBM3D(normalized_noisy_img, original_img, sigma=0.2, specificity=0,noi
     print('Processing BM3D (3/3)',end='\r')
     greater_img = bm3d.bm3d(normalized_noisy_img, sigma_psd=greater_sigma,stage_arg=bm3d.BM3DStages.ALL_STAGES)
     print('Processing BM3D (3/3) Finished')
-    
+
     # Compare PSNR and SSIM test results for each variation with terminal feedback
     norm_image = cv2.normalize(original_img, None, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_32F)
-    
+
     ssim_lesser = calc_ssim(norm_image,lesser_img)
     ssim_starting = calc_ssim(norm_image,img)
     ssim_greater = calc_ssim(norm_image,greater_img)
@@ -63,25 +63,28 @@ def compareBM3D(normalized_noisy_img, original_img, sigma=0.2, specificity=0,noi
             display_results = True
         if specificity == 0:
             print('Increasing Precision')
-            compareBM3D(normalized_noisy_img,original_img,sigma=sigma,specificity=1,noise_type=noise_type)
+            return compareBM3D(normalized_noisy_img,original_img,sigma=sigma,specificity=1,noise_type=noise_type)
     elif greatest == 1:
         print('Increasing sigma by '+str(specificity_value))
-        compareBM3D(normalized_noisy_img,original_img,sigma=greater_sigma,specificity=specificity,noise_type=noise_type)
+        return compareBM3D(normalized_noisy_img,original_img,sigma=greater_sigma,specificity=specificity,noise_type=noise_type)
     else:
         print('Decreasing sigma by '+str(specificity_value))
-        compareBM3D(normalized_noisy_img,original_img,sigma=lesser_sigma,specificity=specificity,noise_type=noise_type)
+        return compareBM3D(normalized_noisy_img,original_img,sigma=lesser_sigma,specificity=specificity,noise_type=noise_type)
 
 
     if display_results:
         print('Finished, close window to continue')
-        # original_img needs to be normalized to display with cv2.imshow()
         cv2.imshow(
             'Before | '+noise_type+' | BM3D '+str(round(sigma,2)),
             np.concatenate((norm_image,normalized_noisy_img,img),axis=1)
             )
         cv2.waitKey()
+        img_out = np.concatenate((norm_image,normalized_noisy_img,img),axis=1)
+        img_out = img_out*255
+        img_out = img_out.astype('uint8')
+        return (img_out , str(round(sigma,2)))
 
-# returns SSIM with most similarity to original 
+# returns SSIM with most similarity to original
 def compareSSIM(less,origin,greater):
     if less > origin and less >= greater:
         return -1
@@ -98,13 +101,13 @@ def addNoise(img, noise_types=['gaussian'],variance=0.2):
     return noisy_images
 
 #This site made these pretty trivial https://cvnote.ddlee.cc/2019/09/12/psnr-ssim-python
-#am smol bran, let me know if i can try to fix this, if no work good.
-# PSNR testing, returns PSNR values from img and post_img comparison 
+# am smol bran, let me know if i can try to fix this, if no work good.
+# PSNR testing, returns PSNR values from img and post_img comparison
 def psnr(img, post_img):
     psnr = cv2.PSNR(img, post_img)
     return psnr
-    
-#this calls ssim, to stay consistent with the implementation I'm basing all of this off of    
+
+#this calls ssim, to stay consistent with the implementation I'm basing all of this off of
 #should this maybe call compareBM3D rather than ssim?
 def calc_ssim(img, post_img):
     if (img.shape == post_img.shape):
@@ -126,11 +129,11 @@ def calc_ssim(img, post_img):
 def ssim(img, post_img):
     C1 = (0.01 * 255) ** 2
     C2 = (0.03 * 255) ** 2
-    
+
     gkernal = cv2.getGaussianKernel(11, 1.5)
     window = np.outer(gkernal, gkernal.transpose())
 
-    mu1 = cv2.filter2D(img, -1, window)[5:-5, 5:-5] 
+    mu1 = cv2.filter2D(img, -1, window)[5:-5, 5:-5]
     mu2 = cv2.filter2D(post_img, -1, window)[5:-5, 5:-5]
     sigma1_sq = cv2.filter2D(img**2, -1, window)[5:-5, 5:-5] - (mu1**2)
     sigma2_sq = cv2.filter2D(post_img**2, -1, window)[5:-5, 5:-5] - (mu2**2)
@@ -144,5 +147,8 @@ def ssim(img, post_img):
 # Medium Noise levels Blobby but maintained areas of interest
 # processImage('Baboon.png',['gaussian'],starting_noise_variance=0.5)
 # Low Noise Levels nearly identical, with few minor details missing
-processImage('Lenna.png',['gaussian'],starting_noise_variance=0.1)
+# processImage('peppers.jpg',['gaussian'],starting_sigma=0.1, starting_noise_variance=0.1)
 
+
+# Demo
+processImage('Lenna.png',['gaussian'],starting_sigma=0.1, starting_noise_variance=0.05)
